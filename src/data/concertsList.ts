2cf783ea4ee8348d6bc7749b60056604e2351127
cagatayyildiz <yildiz.cagatay@hotmail.com>
2024-01-20 13:49:14 +0300

    date filter operations

--- src/data/concertsList.ts
+++ src/data/concertsList.ts
@@ -1,27 +1,28 @@
 export const concertsList = [
     {
         id:1,
-        name: 'Concert 1',
+        name: 'Cem Adrian',
         description: 'Description 1',
-        date: '2020-01-01',
+        date: '22-01-2024',
         image: 'https://picsum.photos/200/300',
         link: 'https://www.google.com',
         location: 'Paris'
     },
     {
         id:2,
-        name: 'Concert 2',
+        name: 'Manuş Baba',
         description: 'Description 2',
-        date: '2020-01-02',
+        date: '24-01-2024',
         image: 'https://picsum.photos/200/300',
         link: 'https://www.google.com',
         location: 'Paris'
     },
     {
         id:3,
-        name: 'Concert 3',
+        name: 'Hayko Cepkin',
         description: 'Description 3',
-        date: '2020-01-03',
+        date: '24-01-2024',
+
         image: 'https://picsum.photos/200/300',
         link: 'https://www.google.com',
         location: 'Paris'
@@ -30,7 +31,7 @@ export const concertsList = [
         id:4,
         name: 'Concert 4',
         description: 'Description 4',
-        date: '2020-01-04',
+        date: '04-02-2024',
         image: 'https://picsum.photos/200/300',
         link: 'https://www.google.com',
         location: 'Paris'
@@ -39,9 +40,14 @@ export const concertsList = [
         id:5,
         name: 'Concert 5',
         description: 'Description 5',
-        date: '2020-01-05',
+        date: '14-02-2024',
         image: 'https://picsum.photos/200/300',
         link: 'https://www.google.com',
         location: 'Paris'
     }
 ]
+
+
+
+// 22.01.2020 -> day, month, year
+// USA Date Format => 01/22/2020  -> day, month, year
--- src/screens/ConcertsScreen.tsx
+++ src/screens/ConcertsScreen.tsx
@@ -1,15 +1,118 @@
 import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
-import { Card } from 'react-native-paper'
+import React, { useState } from 'react'
+import { Button, Card, Divider, TextInput } from 'react-native-paper'
 import { concertsList } from '../data/concertsList'
+import DatePicker from 'react-native-date-picker'
+import dayjs from 'dayjs'
+import 'dayjs/locale/tr'
+
 
 const ConcertsScreen = () => {
+
+  dayjs.locale('tr')
+
+  const [concerts, setconcerts] = useState(concertsList)
+
+  const [startDate, setStartDate] = useState(new Date())
+  const [open, setOpen] = useState(false)
+
+  const [endDate, setendDate] = useState(new Date())
+  const [open2, setOpen2] = useState(false)
+
+
+  const searchByName = (name: string) => {
+
+    var name = name.trim()
+
+    let filteredConcerts = concertsList.filter(item => item.name.toLocaleLowerCase().includes(name.toLocaleLowerCase()))
+
+    setconcerts(filteredConcerts)
+
+  }
+
+
+  const searchByDate = () => {
+
+    let startDateWithFormat = dayjs(startDate).format("DD-MM-YYYY")
+    let endDateWithFormat = dayjs(endDate).format("DD-MM-YYYY")
+
+
+
+    let filteredConcerts = concertsList.filter(item => dayjs(item.date, "DD-MM-YYYY").isAfter(startDateWithFormat) && dayjs(item.date, "DD-MM-YYYY").isBefore(endDateWithFormat))
+
+    setconcerts(filteredConcerts)
+
+  }
+
+
 
   return (<>
     <ScrollView style={{ paddingLeft: '3%', paddingRight: '3%' }}>
 
+      <View style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center' }}>
+
+        <Text>{dayjs(startDate).format('DD MMMM dddd')}</Text>
+        <Button onPress={() => setOpen(true)} mode="contained" style={{ marginBottom: '2%', marginLeft: 5 }}>
+          Başlangıç Tarihi
+        </Button>
+      </View>
+
+      <View style={{ display: 'flex', justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center' }}>
+
+        <Text>{dayjs(endDate).format('DD MMMM dddd')}</Text>
+        <Button onPress={() => setOpen2(true)} mode="contained" style={{ marginBottom: '2%', marginLeft: 5 }}>
+          Bitiş Tarihi
+        </Button>
+      </View>
+      <View>
+        <Button onPress={() => searchByDate()} mode="contained">Tarih Aralığına Göre Ara</Button>
+        <Button onPress={() => setconcerts(concertsList)}>Filtre Temizle</Button>
+      </View>
+
+
+
+      <DatePicker
+        modal
+        open={open}
+        date={startDate}
+        onConfirm={(date) => {
+          setOpen(false)
+          setStartDate(date)
+        }}
+        onCancel={() => {
+          setOpen(false)
+        }}
+        mode="date"
+      />
+
+      <DatePicker
+        modal
+        open={open2}
+        date={endDate}
+        onConfirm={(date) => {
+          setOpen2(false)
+          setendDate(date)
+        }}
+        onCancel={() => {
+          setOpen2(false)
+        }}
+        mode="date"
+
+      />
+
+
+      <Divider style={{ marginBottom: '2%' }} />
+
+
+      <TextInput
+        label="Search by name"
+        onChangeText={text => searchByName(text)}
+        style={{ marginBottom: '2%' }}
+      />
+
       {
-        concertsList.map((item, index) => {
+        concerts.map((item, index) => {
+
           return <Card key={item.id}>
             <Card.Cover source={{ uri: item.image }} />
             <Card.Title titleStyle={{ fontSize: 25 }} title={item.name} subtitle={item.date} />
@@ -17,6 +120,7 @@ const ConcertsScreen = () => {
               <Text>{item.description}</Text>
             </Card.Content>
           </Card>
+
         }
         )
       }
